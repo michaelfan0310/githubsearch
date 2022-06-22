1f2e3e2841733fe2c67647b7c9f13bcde1369153
marshall2023 <74441675+michaelfan0310@users.noreply.github.com>
2022-06-22 16:59:42 -0400

    1st commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>  
     <div class="container">
+        <h1>Test</h1>
        <SearchItem />
        <ListItem />
     </div> 
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@
 //   transpileDependencies: true
 // })
 
+// vue.config.js file to be placed in the root of your repository
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
